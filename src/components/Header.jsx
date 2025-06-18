e8869ed553c2bdfe93396912ee4da9cc066c5ae7
PietroMelle <mellepietro3@gmail.com>
2025-06-18 10:10:12 -0300

    header finalizado

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,23 +1,31 @@
 import { Link } from "react-router-dom";
 
-import '@fortawesome/fontawesome-free/css/all.min.css'; // Importando a instalação do Font Awesome (Para usar os icones)
-import "./styles/Header.css"
+import "@fortawesome/fontawesome-free/css/all.min.css"; // Importando a instalação do Font Awesome (Para usar os icones)
+// npm install @fortawesome/fontawesome-free
+
+import "./styles/Header.css";
 
 function Header() {
-    return(
-        <>
-        <section className="HeaderPrincipal">
-            <i class="fa-brands fa-github"></i>
-            <img src="../../public/Marvel_Logo.png" alt="Marvel Logo" />
-        </section>
+  return (
+    <>
+      <section className="HeaderPrincipal">
+        <a href="https://github.com/vitorcecato-sesi/projeto-marvelAPI" target="_blank" rel="noopener noreferrer">
+          <i className="fa-brands fa-github"></i> {/* Icone do GitHub */}
+        </a>
 
-        <nav className="Navbar">
-            <Link to="/"> HOME </Link>
-            <Link to="/"> FAVORITOS </Link>
-        </nav>
-        
-        </>
-    )
+        <a href="https://www.marvel.com/" target="_blank" rel="noopener noreferrer">
+          <img src="../../public/Marvel_Logo.png" alt="Marvel Logo" /> {/* Imagem Logo da Marvel */}
+        </a>
+
+        <h3> Find Your Super Hero</h3>
+      </section>
+
+      <nav className="Navbar">
+        <Link to="/Home"> HOME </Link>
+        <Link to="/Favoritos"> FAVORITOS </Link>
+      </nav>
+    </>
+  );
 }
 
-export default Header
+export default Header;
